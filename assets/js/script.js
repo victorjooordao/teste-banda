4ddd1addce1d7f20123d231b5fc8db52ac204160
victorjooordao <102872696+victorjooordao@users.noreply.github.com>
2023-08-24 03:41:56 -0300

    commit css js

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const videoYtbLazy = document.getElementById('videoYtbLazy');
+
+videoYtbLazy.addEventListener('click', snippetYtbLazyPlay);
+function snippetYtbLazyPlay(evt) {
+    evt.currentTarget.removeEventListener('click', snippetYtbLazyPlay)
+
+    evt.currentTarget.innerHTML=`<iframe src="https://www.youtube.com/embed/${evt.currentTarget.dataset.id}?autoplay=1" title="O GTA 6 Está Pronto...." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+}
