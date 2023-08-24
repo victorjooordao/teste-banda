4c7f1936c5e9c7b44ea13b694420cc452eff6502
victorjooordao <102872696+victorjooordao@users.noreply.github.com>
2023-08-24 20:23:47 -0300

    com videos banda

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,5 +4,5 @@ videoYtbLazy.addEventListener('click', snippetYtbLazyPlay);
 function snippetYtbLazyPlay(evt) {
     evt.currentTarget.removeEventListener('click', snippetYtbLazyPlay)
 
-    evt.currentTarget.innerHTML=`<iframe src="https://www.youtube.com/embed/${evt.currentTarget.dataset.id}?autoplay=1" title="O GTA 6 Está Pronto...." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+    evt.currentTarget.innerHTML=`<iframe width="720" height="1280" src="https://www.youtube.com/embed/${evt.currentTarget.dataset.id}?autoplay=1" title="O GTA 6 Está Pronto...." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
 }
